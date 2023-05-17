17b100c15de43a3f776e09b7ddf87aa0036ccff3
cysulfur <133861188+cysulfur@users.noreply.github.com>
2023-05-17 13:11:17 +0300

    vs project

---
+++ directx-project/source/Application.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <windows.h>
+
+int CALLBACK WinMain
+(
+	HINSTANCE	hInstance,
+	HINSTANCE	hPrevInstance,
+	LPSTR		lpCmdLine,
+	int			nShowCmd
+)
+{
+	while (true);
+	return 0;
+}
