1570933ecc862d676dc99f7f8217c83571f542aa
cysulfur <133861188+cysulfur@users.noreply.github.com>
2023-05-18 17:24:33 +0300

    basic win32 app

--- directx-project/source/Application.cpp
+++ directx-project/source/Application.cpp
@@ -1,14 +1,77 @@
 #include <string>
 #include <windows.h>
+#include <sstream>
 
-int CALLBACK WinMain
-(
-	HINSTANCE	hInstance,
-	HINSTANCE	hPrevInstance,
-	LPSTR		lpCmdLine,
-	int			nShowCmd
-)
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	switch (uMsg) {
+	case WM_CLOSE:
+		PostQuitMessage(69);
+		break;
+	case WM_KEYDOWN:
+		if (wParam == 'F') {
+			SetWindowText(hWnd, L"1");
+		}
+		break;
+	case WM_KEYUP:
+		if (wParam == 'F') {
+			SetWindowText(hWnd, L"0");
+		}
+		break;
+	case WM_CHAR:
+		if (wParam == 'f') {
+			SetWindowText(hWnd, L"Lowercase f");
+		}
+		break;
+	case WM_LBUTTONDOWN:
+	{
+		const POINTS pts = MAKEPOINTS(lParam);
+		std::wstringstream wss;
+		wss << L"(" << pts.x << L"," << pts.y << L")" << L"\n";
+		SetWindowText(hWnd, wss.str().c_str());
+		break;
+	}
+	default:
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+		break;
+	}
+}
+
+int CALLBACK WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	while (true);
-	return 0;
+	const auto className = L"Application Class";
+
+	WNDCLASSEX wc{};
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = className;
+
+	RegisterClassEx(&wc);
+
+	HWND hWnd = CreateWindowExW(WS_EX_LEFT, className, L"My Window",
+		WS_BORDER | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 800, 600,
+		nullptr, nullptr, hInstance, nullptr);
+
+	if (hWnd == NULL)
+	{
+		return -1;
+	}
+
+	ShowWindow(hWnd, SW_SHOW);
+
+	MSG msg;
+	BOOL bRet;
+	while (bRet = GetMessage(&msg, nullptr, 0, 0) > 0) {
+		//TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	if (bRet == -1) {
+		return -1;
+	}
+	else {
+		return (int) msg.wParam;
+	}
+
 }
